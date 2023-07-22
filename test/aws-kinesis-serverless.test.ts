1e1b2cfcedbd8456dbe90c348b2e78d5c97a223b
entest <entest>
2023-07-22 11:24:56 +0700

    Initial commit

---
+++ test/aws-kinesis-serverless.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsKinesisServerless from '../lib/aws-kinesis-serverless-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-kinesis-serverless-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsKinesisServerless.AwsKinesisServerlessStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
